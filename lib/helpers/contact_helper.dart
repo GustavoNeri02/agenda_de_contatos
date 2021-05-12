3fb6b6083337dd92e93cbddf2c97a78a212594b6
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-05-12 16:55:39 -0300

    Inicializando o banco de dados * get database * initDataBase

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,3 +1,7 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+final String contactTable = "contactTable";
 //endereços do map
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
@@ -5,7 +9,38 @@ final String emailColumn = "emailColumn";
 final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
-class ContactHelper {}
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contacts.db");
+
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db
+          .execute("CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, "
+              "$nameColumn TEXT, "
+              "$emailColumn TEXT, "
+              "$phoneColumn TEXT, "
+              "$imgColumn TEXT)");
+    });
+  }
+}
 
 //id name email phone img
 //0 gustavo gutneri@hotmail.com (31)99576-9061 imagem.png
