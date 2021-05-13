39356c83ce82597cf48aa5d81b87748b90ec97d4
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-05-13 12:21:48 -0300

    Criado tela principal *Scaffold *ListView.builder *Card - GestureDetector

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:agenda_de_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -10,23 +12,79 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   ContactHelper helper = ContactHelper();
+  List<Contact> listContacts = [];
 
+  //teste
   @override
   void initState() {
     super.initState();
-    // Contact c = Contact();
-    // c.name = "Gustavo Neri";
-    // c.email = "gutneri@hotmail.com";
-    // c.phone = "(77) 4615";
-    // c.img = "sdsadsa";
-    // helper.saveContact(c);
-    helper.getAllContacts().then((list) => {print(list)});
-
+    setState(() {
+      helper.getAllContacts().then((list) => {listContacts = list});
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Contatos",
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [IconButton(icon: Icon(Icons.more_vert), onPressed: () {})],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: listContacts.length,
+          itemBuilder: (context, index) {
+            return contactCard(context, index);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget contactCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: listContacts[index].img != null
+                          ? FileImage(File(listContacts[index].img))
+                          : AssetImage("images/person.png")),
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(right: 10)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(listContacts[index].name ?? "",
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                  Text(listContacts[index].email ?? "",
+                      style: TextStyle(fontSize: 18)),
+                  Text(listContacts[index].phone ?? "",
+                      style: TextStyle(fontSize: 18)),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
